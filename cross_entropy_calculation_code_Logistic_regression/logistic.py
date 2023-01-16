6b680644a3196b765e8d96e63920ab471e464373
Aditya Kumar <aryan68125@gmail.com>
2023-01-16 14:19:45 +0530

    added some comments on logistic regression

--- cross_entropy_calculation_code_Logistic_regression/logistic.py
+++ cross_entropy_calculation_code_Logistic_regression/logistic.py
@@ -3,6 +3,8 @@ import numpy as np
 N=100 # Number of samples
 D=2 # Number of dimensions or features
 
+# X = data that we need to perform logistic regression on
+# X = normally distributed data matrix
 X = np.random.randn(N,D) # matrix of data N by D or samples by features in each samples
 
 # this times we are gonna have labels because we want to calculate the error
@@ -15,14 +17,23 @@ X[50:,:]=X[50:,:] + 2*np.ones((50,D)) #centered at +2,+2
 T = np.array([0]*50+[1]*50)
 
 # concatenate a column with 1s or biase
+# now we know that we are gonna have to add a bias term. in order to do that we just gonna add the column of ones to the original data and include
+# the bias term in the weights w.
+# An array in numpy is only one Dimensional and we need it to be 2 dimensional in order to have n rows and two column
+# So now I am gonna concatenate the array of ones to my original dataset by "Xb = np.concatenate((ones,X),axis=1)"
 ones = np.array([[1]*N]).T
 Xb = np.concatenate((ones,X),axis=1)
 
 #randomly initialize the weights
 w = np.random.randn(D+1)
+
 #calculate the model output
+# calculate the dot product between each row of X and W "z=Xb.dot(w)"
 z=Xb.dot(w)
 
+#sigmoid function
+# NOTE : Numpy works on vectors as well as scalers so you need to pass it through the function once
+# here you will get the values will be in between 0 and 1 and our output is n by 1 matrix
 def sigmoid(z):
     return 1/(1+np.exp(-z))
 
