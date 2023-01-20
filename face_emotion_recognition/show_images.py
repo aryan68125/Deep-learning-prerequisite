8ff22e573ba2da82a4ede2148296663df877d303
Aditya Kumar <aryan68125@gmail.com>
2023-01-20 11:47:39 +0530

    added some comment so that we can undertsand the code better

--- face_emotion_recognition/show_images.py
+++ face_emotion_recognition/show_images.py
@@ -4,16 +4,25 @@ from util import getData
 
 label_map = ['Anger', 'Disgust' , 'Fear' , 'Happy' , 'Sad' , 'Surprise' , 'Neutral']
 def main():
+    '''
+    It loads the data and preprocesses it
+    '''
     X,Y = getData(balance_ones = False)
     while True :
         for i in range(7):
             x,y = X[Y==i], Y[Y==i]
             N = len(y)
             j = np.random.choice(N)
+            '''
+             Color map to grey since it's a grey scale image.
+            '''
             plt.imshow(x[j].reshape(48,48),cmap = 'gray')
             plt.title(label_map[y[j]])
             plt.show()
-        prompt = raw_input('Quite? Enter Y:\n')
+        '''
+        Because it's an infinite loop We are going to write the code to promp the user to quite the loop.
+        '''
+        prompt = input('Quite? Enter Y:\n')
         if prompt == 'Y':
             break
 if __name__ == '__main__':
