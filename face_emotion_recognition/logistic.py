5c40f2d85e3b5f73b8ad04f12a89f62d3e51a9b2
Aditya Kumar <aryan68125@gmail.com>
2023-01-20 11:27:59 +0530

    Added some comments to better understand weight initialization

--- face_emotion_recognition/logistic.py
+++ face_emotion_recognition/logistic.py
@@ -21,6 +21,29 @@ class LogisticModel(object):
         X, Y = X[:-1000],Y[:-1000]
         N,D = X.shape
         #initialize the weights
+        '''
+         Why are we dividing the weights during initialization by the square root of input dimensionality
+         self.W = np.random.randn(D)/np.sqrt(D)
+         As you know poor weight initialization may or may not lead to poor convergence of your loss per interation. Sometimes your loss
+         may even explode to infinity.
+         NOTE : It will not work all the time also in some cases it will perform poorly.
+
+         How it works?
+         self.W = np.random.randn(D)/np.sqrt(D)
+         randn() == draws samples from the standard normal and that's usually denoted as N(0,1)
+         what happens when we multiply these samples by some number C. Well you can show thaat if you multiply random samples from the standard
+         normal by a number c they will have mean zero and standard deviation c. In other words it's equivalent to sampling from the distribution
+         and a zero c^2 N(0,C^2). Infact this is just the opposite of standardization and normalization.
+         To standardize : Z = (X - mue)/ sigma (where mue is = mean and sigma = standard deviation).
+
+         What does this code do?
+         self.W = np.random.randn(D)/np.sqrt(D) means that we want weight w to have the mean 0 and variance 1/D.
+         So why would we want to do that? If you don't do this then your loss per iteration may explode. well the reason it's exploding
+         because those weights are just too large and if the weights are too large then that means their variance is too large.
+         Which means we might be able to avoid this problem if we make the variance smaller. In some code people just multiply the weights
+         by some constant like 0.01. Experimentation is the key there is no fixed rule to determine the initialization of the weights.
+         It all depends on the data that you are working.
+        '''
         self.W = np.random.randn(D)/np.sqrt(D)
         self.b = 0
 
