c2cb7e0a65c1bd606bc33dc36e65bc0d2cb33cc5
aryan68125 <aryan68125@gmail.com>
2025-07-08 10:02:56 +0530

    Slicing in numpy array

--- numpy/numpy_stacking_slicing.py
+++ numpy/numpy_stacking_slicing.py
@@ -20,6 +20,15 @@ np_2d_array = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 print(f"printing numpy 2D array ===> \n {np_2d_array}")
 # np_2d_array[row, column]
 print(f"select element in numpy 2D array: {np_2d_array[1, 1]}")  # Accessing element at row 0, column 1
+# slicing in numpy 2D array
+# np_2d_array[row,column] here select row = 0:2 column = 2 hence the code will select 3 and 6 since these elements are in 0th and 1st row and in column 2
+# when slicing a 2D array like this 0:2 the code doesn't include the number 2 which means it will select the element from 0th to 1st row this is the same when you slice the columns as well
+print(f"slice a 2D array in a way to select the 2nd element in row 0 and row1 : {np_2d_array[0:3,2]}")
+print(f"slicing a 2D array to select the first two column from the first row : {np_2d_array[0,1:3]}")
+# you can also do reverse indexing in numpy 2D array
+print(f"reverse indexing in numpy 2D array: {np_2d_array[-1,-1]}")  # Accessing the last element in the last row
+# if you provie the reverse indexing like this then it will select the last row from the 2D array
+print(f"reverse indexing to select last row from the 2D array: {np_2d_array[-1]}")
 """2. ITERATING THROUGH A NUMPY ARRAY"""
 """3. STACKING TOGEATHER TWO ARRAYS"""
 """4. INDEXING A BOOLEAN NUMPY ARRAY"""
