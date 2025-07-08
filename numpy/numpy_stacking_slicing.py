51a43b85c18a114c2bc1593ac6a0c1b79c387aaf
aryan68125 <aryan68125@gmail.com>
2025-07-08 09:39:48 +0530

    Added indexing in python list demo

--- numpy/numpy_stacking_slicing.py
+++ numpy/numpy_stacking_slicing.py
@@ -1,6 +1,9 @@
 import numpy as np
 
 """1. INDEXING AND SLICING"""
+# indexing in python list
+my_list = [1, 2, 3, 4, 5]
+print(f"indexing in python list: {my_list[0:3]}")
 """2. ITERATING THROUGH A NUMPY ARRAY"""
 """3. STACKING TOGEATHER TWO ARRAYS"""
 """4. INDEXING A BOOLEAN NUMPY ARRAY"""
