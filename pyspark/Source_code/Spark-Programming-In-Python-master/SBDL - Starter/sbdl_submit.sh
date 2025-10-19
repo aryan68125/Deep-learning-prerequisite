7176256dad998451bd6ac7606c88d744aa59e59e
Aditya Kumar <aryan68125@gmail.com>
2025-10-19 14:31:55 +0530

    New changes

--- pyspark/Source_code/Spark-Programming-In-Python-master/SBDL - Starter/sbdl_submit.sh
+++
@@ -1,7 +0,0 @@
-spark-submit --master yarn --deploy-mode cluster \
---py-files sdbl_lib.zip \
---files conf/sdbl.conf,conf/spark.conf,log4j.properties \
-sbdl_main.py qa 2022-08-02
-
-
-
--- pyspark/Source_code/Spark-Programming-In-Python-master/SBDL/sbdl_submit.sh
+++
@@ -1,7 +0,0 @@
-spark-submit --master yarn --deploy-mode cluster \
---py-files sdbl_lib.zip \
---files conf/sdbl.conf,conf/spark.conf,log4j.properties \
---driver-cores 2 \
---driver-memory 3G \
---conf spark.driver.memoryOverhead=1G
-sbdl_main.py qa 2022-08-03
