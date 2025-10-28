99173692512582d2f1a78d9c93a5483276f50f17
Aditya Kumar <aryan68125@gmail.com>
2025-10-28 15:46:37 +0530

    Added new theory

--- pyspark/Test_Program/pyspark_test.py
+++ pyspark/Test_Program/pyspark_test.py
@@ -1,6 +1,12 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, rand
 
+# For keeping track of time taken to complete the spark computation task
+import time
+import sys
+
+start = time.time()
+
 # 1️⃣ Create Spark session in local mode using all cores
 spark = SparkSession.builder \
     .appName("CPU_Stress_Test") \
@@ -11,11 +17,27 @@ print("Spark master:", spark.sparkContext.master)
 print("Total cores Spark sees:", spark.sparkContext.defaultParallelism)
 
 # 2️⃣ Create a large synthetic dataset (e.g., 100 million rows)
-num_rows = 999_999_999
+num_rows = 999_999_999_999
 num_partitions = spark.sparkContext.defaultParallelism  # same as CPU cores
 
 df = spark.range(0, num_rows, numPartitions=num_partitions) \
            .withColumn("random_val", rand())
+
+# I want to know the amount of Ram taken by the dataFrame in Mbs
+# converting df to rdd rows
+rdd = df.rdd.map(lambda row: row.asDict())
+# Estimate memory size of one partition
+def estimate_partition_size(partition):
+    import sys
+    size = 0
+    for record in partition:
+        size += sys.getsizeof(record)
+    yield size
+
+partition_sizes = rdd.mapPartitions(estimate_partition_size).collect()
+total_bytes = sum(partition_sizes)
+total_mb = total_bytes / (1024 * 1024)
+print(f"Estimated DataFrame size in memory: {total_mb:.2f} MB")
 
 # 3️⃣ Apply heavy transformations — wide operations
 #    Force Spark to use multiple stages and shuffles
@@ -28,3 +50,13 @@ aggregated_df = (
 
 # 4️⃣ Trigger computation (action)
 aggregated_df.show()
+
+end = time.time()
+
+time_taken_in_sec = end - start
+time_taken_in_min = (end - start) / 60
+print(f"""
+        Spark task complete!
+        Time taken in seconds = {time_taken_in_sec}
+        Time taken in minutes = {time_taken_in_min}
+""")
