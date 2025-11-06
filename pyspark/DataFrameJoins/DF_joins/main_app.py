23c717c6b8a07dc69045894b4c05067a5ede1181
Aditya Kumar <aryan68125@gmail.com>
2025-11-06 12:12:30 +0530

    Added a new code that allows us to prevent column ambiguity

--- pyspark/DataFrameJoins/DF_joins/main_app.py
+++ pyspark/DataFrameJoins/DF_joins/main_app.py
@@ -120,7 +120,7 @@ if __name__ == "__main__":
     # performing inner join opertion on the two dataFrames
     join_expression = generated_order_df.prod_id == product_renamed_df.prod_id2
     inner_join_df = df_joins.inner_join_df(left_df=generated_order_df,right_df=product_renamed_df,join_expression=join_expression)
-    inner_join_df = inner_join_df.select("order_id","prod_id","unit_price","qty","prod_name","list_price","reorder_qty")
+    inner_join_df = inner_join_df.drop("prod_id2")
     # logging dataFrame
     sp_df_logger.log_df(spark_df=inner_join_df,spark_df_name="inner_join_df")
 
@@ -129,7 +129,7 @@ if __name__ == "__main__":
     # performing left join operation on the two dataFrames
     join_expression = generated_order_df.prod_id == product_renamed_df.prod_id2
     left_join_df = df_joins.left_join_df(left_df=generated_order_df,right_df=product_renamed_df,join_expression=join_expression)
-    left_join_df = left_join_df.select("order_id","prod_id","unit_price","qty","prod_name","list_price","reorder_qty")
+    left_join_df = left_join_df.drop("prod_id2")
     # logging dataFrame
     sp_df_logger.log_df(spark_df=left_join_df,spark_df_name="left_join_df")
 
@@ -138,7 +138,7 @@ if __name__ == "__main__":
     # performing right join operation on the two dataframes
     join_expression = generated_order_df.prod_id == product_renamed_df.prod_id2
     right_join_df = df_joins.right_join_df(left_df=generated_order_df,right_df=product_renamed_df,join_expression=join_expression)
-    right_join_df = right_join_df.select("order_id","prod_id","unit_price","qty","prod_name","list_price","reorder_qty")
+    right_join_df = right_join_df.drop("prod_id2")
     # logging dataFrame
     sp_df_logger.log_df(spark_df=right_join_df,spark_df_name="right_join_df")
 
@@ -147,7 +147,7 @@ if __name__ == "__main__":
     # performing outer join operation on the two dataFrames
     join_expression = generated_order_df.prod_id == product_renamed_df.prod_id2
     outer_join_df = df_joins.outer_join_df(left_df=generated_order_df,right_df=product_renamed_df,join_expression=join_expression)
-    outer_join_df = outer_join_df.select("order_id","prod_id","unit_price","qty","prod_name","list_price","reorder_qty")
+    outer_join_df = outer_join_df.drop("prod_id2")
     # logging dataFrame
     sp_df_logger.log_df(spark_df=outer_join_df,spark_df_name="outer_join_df")
     """Join operation ENDS"""
