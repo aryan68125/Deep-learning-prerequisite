aa3f3a10946ea9fdc5e9518d3bb24e65d5c0d5eb
Aditya Kumar <aryan68125@gmail.com>
2025-11-07 16:08:38 +0530

    Completed the project

--- pyspark/Pyspark_project/project_typeR/lib/Utils.py
+++ pyspark/Pyspark_project/project_typeR/lib/Utils.py
@@ -1,9 +1,13 @@
 from pyspark.sql import SparkSession
+from lib.ConfigLoader import get_spark_conf
 
 
 def get_spark_session(env):
     if env == "LOCAL":
         return SparkSession.builder \
+            .config(conf=get_spark_conf(env)) \
+            .config('spark.sql.autoBroadcastJoinThreshold',-1) \
+            .config('spark.sql.adaptive.enabled','false') \
             .config('spark.driver.extraJavaOptions',
                     '-Dlog4j.configuration=file:log4j.properties') \
             .master("local[2]") \
@@ -11,7 +15,6 @@ def get_spark_session(env):
             .getOrCreate()
     else:
         return SparkSession.builder \
+            .config(conf=get_spark_conf(env)) \
             .enableHiveSupport() \
             .getOrCreate()
-
-
--- pyspark/Pyspark_project/project_typeR/sbdl_submit.sh
+++ pyspark/Pyspark_project/project_typeR/sbdl_submit.sh
@@ -1,7 +1,7 @@
 spark-submit --master yarn --deploy-mode cluster \
 --py-files sdbl_lib.zip \
 --files conf/sdbl.conf,conf/spark.conf,log4j.properties \
-sbdl_main.py qa 2022-08-02
-
-
-
+--driver-cores 2 \
+--driver-memory 3G \
+--conf spark.driver.memoryOverhead=1G
+sbdl_main.py qa 2022-08-03
